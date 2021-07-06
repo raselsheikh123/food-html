5da800a06ccb2881cc3810d3742b9734f07c1549
xrasel81 <raselsheikh123@gmail.com>
2021-07-07 00:50:53 +0600

    owl carousel add

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,4 +5,24 @@ $(document).ready(function(){
         time: 2000
       });
 
+      
+      $("#owl-demo").owlCarousel({
+ 
+        navigation : true, // Show next and prev buttons
+         
+        slideSpeed : 300,
+        paginationSpeed : 400,
+         
+        items : 1, 
+        itemsDesktop : false,
+        itemsDesktopSmall : false,
+        itemsTablet: false,
+        itemsMobile : false,
+        autoPlay:true,
+        navigation:false,
+        paginationNumbers:false,
+        pagination:false
+         
+        });
+
 });
